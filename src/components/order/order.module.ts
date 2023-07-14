5abb4d3b3b4187aa35b8686088ae4c3a8a773e72
mahdiazizzadeh <yasermahdiazizzadeh@gmail.com>
2023-07-14 21:03:15 +0330

    model dependencies injected

--- src/components/order/order.module.ts
+++ src/components/order/order.module.ts
@@ -3,10 +3,16 @@ import { OrderService } from "./order.service";
 import { OrderResolver } from "./order.resolver";
 import { MongooseModule } from "@nestjs/mongoose";
 import ordersSchema, { Orders } from "../../models/orders.model";
+import shippingSchema, { Shipping } from "../../models/shipping.model";
+import billingSchema, { Billing } from "../../models/billing.model";
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Orders.name, schema: ordersSchema }]),
+    MongooseModule.forFeature([
+      { name: Orders.name, schema: ordersSchema },
+      { name: Shipping.name, schema: shippingSchema },
+      { name: Billing.name, schema: billingSchema },
+    ]),
   ],
   providers: [OrderResolver, OrderService],
 })
